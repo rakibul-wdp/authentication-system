ee7495df58d6d3f914d1e4027f0078ef4c232205
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-07 13:00:35 +0600

    fix some bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import Login from './components/auth/Login';
+import RequireAuth from './components/auth/RequireAuth';
+import SignUp from './components/auth/SignUp';
+import MyProfile from './components/profile/MyProfile';
+import Navbar from './components/shared/Navbar';
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <RequireAuth>
+              <MyProfile />
+            </RequireAuth>
+          }
+        />
+        <Route path='login' element={<Login />} />
+        <Route path='signup' element={<SignUp />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/profile/MyProfile.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyProfile = () => {
+  return (
+    <div>
+      <h1>My Profile here...!!!!</h1>
+    </div>
+  );
+};
+
+export default MyProfile;
