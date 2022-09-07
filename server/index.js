e1db77d78b4d52653b4f3e567d6a1365f5ec678e
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-07 15:39:24 +0600

    mongodb database connected

--- server/index.js
+++ server/index.js
@@ -3,10 +3,28 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lunpegw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('database connected');
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello Auth System...!!!');
