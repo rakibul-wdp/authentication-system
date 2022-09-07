5d4b8eae3b94c3ec1ddee31ffb6cf72a083e80ad
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-07 16:55:23 +0600

    load user with token in client with custom hook

--- server/index.js
+++ server/index.js
@@ -32,9 +32,8 @@ async function run() {
         $set: user,
       };
       const result = await userCollection.updateOne(filter, updateDoc, options);
-      // const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-      // res.send({ result, token });
-      res.send(result);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+      res.send({ result, token });
     });
   } finally {
   }
