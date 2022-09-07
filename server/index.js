37a595cc53bed62feb5ec8a9e7ae7d92dea8a3ab
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-07 14:31:15 +0600

    initialize server setup

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello Auth System...!!!');
+});
+
+app.listen(port, () => {
+  console.log(`Auth System listening on port ${port}`);
+});
