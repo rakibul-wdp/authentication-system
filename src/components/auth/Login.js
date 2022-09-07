dd9e984d20199995132fd7cc80b5382eeae7199b
rakibul-wdp <rakibul.wdp@gmail.com>
2022-09-07 11:56:51 +0600

    add login component

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -7,6 +7,7 @@ import {
 import { useForm } from 'react-hook-form';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import Loading from '../shared/Loading';
 
 const Login = () => {
   const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
